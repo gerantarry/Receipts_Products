758ec4c697484c301073a97059007e91095b12ce
Anton <geraskin.antony@yandex.ru>
2022-02-02 13:37:29 +0700

    -инициализация кнопок в активити

--- app/src/main/java/android/receiptsproducts/MainActivity.kt
+++ app/src/main/java/android/receiptsproducts/MainActivity.kt
@@ -2,10 +2,18 @@ package android.receiptsproducts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var productsButton:Button
+    private lateinit var receiptsButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        productsButton = findViewById(R.id.go_to_products_button)
+        receiptsButton = findViewById(R.id.go_to_receipts_button)
+
     }
 }
